3c8378658c572aa371b34d5916f01ec8c6028b99
Michael Shyu <michaelshyu11@gmail.com>
2023-11-06 21:27:08 -0500

    added required functions

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+
+const employee = {
+    
+    name : "burger king",
+    streetAddress : "1010 the street"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj = {
+        ...employee,
+        [key] : value
+    };
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key]= value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newObj = {...employee};
+
+    delete newObj[key];
+
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+
+    return employee;
+}
